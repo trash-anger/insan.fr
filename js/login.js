d84cee7769b41e08f11a3220efe71d7b8c2f415e
Florian Frouin <florian.frouin@bnc.ca>
2019-10-01 12:37:46 -0400

    show test

--- js/login.js
+++ js/login.js
@@ -142,7 +142,12 @@ const options = {
 
 const lockPasswordless = new Auth0LockPasswordless('JTRj7z5QVxX3uqRqVI8X9GuhOYsVKm34', 'insan.eu.auth0.com', options);
 console.log(lockPasswordless)
-localStorage.setItem('state', lockPasswordless.state);
+lockPasswordless.checkSession({}, function(err, authResult) {
+    console.log(authResult.state)
+    localStorage.setItem('state', authResult.state);
+
+});
+
 lockPasswordless.show();
 
 $(document).ready(function () {
